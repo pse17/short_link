ac3f020cac262890efb78b3b7f9201644d5ed25f
pse17 <fis777@yandex.ru>
2020-10-27 16:42:09 +0500

    press to one file

--- short_link.py
+++ short_link.py
@@ -1,40 +1,61 @@
 from flask import Flask
-import hashids
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
+import os
 
 app = Flask(__name__)
 
+
+# Config 
+env = os.environ.get('FLASK_ENV')
+if env == 'development':
+    app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///links.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Model
+db = SQLAlchemy(app)
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(255), unique=True, nullable=False)
+    suffix = db.Column(db.String(6), unique=True, nullable=False)
+    count = db.Column(db.Integer)
+
+
+# Shema
+class LinkSchema(Schema):
+    id = fields.Int()
+    long_url = fields.Str()
+    suffix = fields.Str()
+    count = fields.Int()
+    short_link = fields.Method("make_short_link", dump_only=True)
+
+    def make_short_link(self, link):
+        return "http://mydomen.ru/{}".format(link.suffix)
+
+only_link_schema = LinkSchema(only=('long_url',))
+only_count_schema = LinkSchema(only=('count',))
+only_short_link_schema = LinkSchema(only=('short_link',))
+
+
+# API
 @app.route('/')
 def help():
-    return '/long_to_short - get shortlink, /\<short-link\> - redirect to this link,\
-         /statistics/<short-link> -the number of clicks on this short link'
+    message = r'''Valid is: /long_to_short - get shortlink, /<short_postfix> - redirect to link whth this postfix,
+         /statistics/<short_postfix> -the number of clicks on short link whth this postfix'''
+    return {"message": message}, 400
 
 
 @app.route('/long_to_short')
 def long_to_short():
-    pass
+    return {}
 
 
-@app.route('/statistics/<short_postfix>')
+@app.route('/<short_postfix>')
 def redirect_to_long_link(short_postfix):
-    pass
+    return {}
 
 
 @app.route('/statistics/<short_postfix>')
 def statistics(short_postfix):
-    pass
-
-
-
-
-
-'''
-hashids = hashids.Hashids(
-    salt="Salt is white poison", 
-    min_length=6,
-    alphabet='abcdefghijklmnopqrstuvwxyz1234567890')
-
-hash_pk = hashids.encode(223)
-print(hash_pk)
-pk = hashids.decode(hash_pk)
-print(pk)
-'''
+    return {}
